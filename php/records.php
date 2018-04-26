057541b289cef97cd2bdf7bb618707995e2cd29c
g4bleb <gableb@hotmail.fr>
2018-04-26 17:55:52 +0200

    records now refreshes (dirty js)

--- php/records.php
+++ php/records.php
@@ -51,42 +51,42 @@ $recordsList = $records->fetchAll(PDO::FETCH_CLASS, 'Parametre');
         $selectedRecord = $tabselectedRecord[0];
 
         echo "<form action='records.php' method='GET'>
-              <input type='hidden' name='id' value='".$_GET['id']."'>
-             Nom ? <input type='text' name='libelle' value='".$selectedRecord->getLibelle()."'><br>
-             Taille de la corde ?  <input type='text' name='corde' value='".$selectedRecord->getCorde()."'><br>
-             Nombre de points ? <input type='text' name='nb_points' value='".$selectedRecord->getNb_Points()."'><br>
-             Epaisseur max en % ?  <input type='text' name='tmax_prc' value='".$selectedRecord->getTmax_prc()."'><br>
-             Cambrure max en % ? <input type='text' name='fmax_prc' value='".$selectedRecord->getFmax_prc()."'><br>
-             <input type='submit' name='mode' value='Editer'>
+        <input type='hidden' name='id' value='".$_GET['id']."'>
+        Nom ? <input type='text' name='libelle' value='".$selectedRecord->getLibelle()."'><br>
+        Taille de la corde ?  <input type='text' name='corde' value='".$selectedRecord->getCorde()."'><br>
+        Nombre de points ? <input type='text' name='nb_points' value='".$selectedRecord->getNb_Points()."'><br>
+        Epaisseur max en % ?  <input type='text' name='tmax_prc' value='".$selectedRecord->getTmax_prc()."'><br>
+        Cambrure max en % ? <input type='text' name='fmax_prc' value='".$selectedRecord->getFmax_prc()."'><br>
+        <input type='submit' name='mode' value='Editer'>
         </form>";
 
         if(isset($_GET["libelle"]) && isset($_GET["corde"]) && isset($_GET["nb_points"]) && isset($_GET["tmax_prc"]) && isset($_GET["fmax_prc"])){
 
-             $selectedRecord->setCorde($_GET["corde"]);
-             $selectedRecord->createDate();
-             $selectedRecord->setTmax_prc($_GET["tmax_prc"]);
-             $selectedRecord->setFmax_prc($_GET["fmax_prc"]);
-             $selectedRecord->tmax_prc_to_mm();
-             $selectedRecord->fmax_prc_to_mm();
+          $selectedRecord->setCorde($_GET["corde"]);
+          $selectedRecord->createDate();
+          $selectedRecord->setTmax_prc($_GET["tmax_prc"]);
+          $selectedRecord->setFmax_prc($_GET["fmax_prc"]);
+          $selectedRecord->tmax_prc_to_mm();
+          $selectedRecord->fmax_prc_to_mm();
 
-             $editRecord = $dbCnx->prepare("UPDATE parametre SET libelle=:libelle, corde=:corde, tmax_prc=:tmax_prc, tmax_mm=:tmax_mm, fmax_prc=:fmax_prc, fmax_mm=:fmax_mm, nb_points=:nb_points, date=:stringDate WHERE id=:id");
+          $editRecord = $dbCnx->prepare("UPDATE parametre SET libelle=:libelle, corde=:corde, tmax_prc=:tmax_prc, tmax_mm=:tmax_mm, fmax_prc=:fmax_prc, fmax_mm=:fmax_mm, nb_points=:nb_points, date=:stringDate WHERE id=:id");
 
-             $editRecord->execute(array(
-                  ':libelle'=>$_GET["libelle"],
-                  ':corde'=>$_GET["corde"],
-                  ':tmax_prc'=>$_GET["tmax_prc"],
-                  ':tmax_mm'=>$selectedRecord->getTmax_mm(),
-                  ':fmax_prc'=>$_GET["tmax_prc"],
-                  ':fmax_mm'=>$selectedRecord->getFmax_mm(),
-                  ':nb_points'=>$_GET["nb_points"],
-                  ':stringDate'=>$selectedRecord->getDate(),
-                  ':id'=>$selectedRecord->getId()));
+          $editRecord->execute(array(
+            ':libelle'=>$_GET["libelle"],
+            ':corde'=>$_GET["corde"],
+            ':tmax_prc'=>$_GET["tmax_prc"],
+            ':tmax_mm'=>$selectedRecord->getTmax_mm(),
+            ':fmax_prc'=>$_GET["tmax_prc"],
+            ':fmax_mm'=>$selectedRecord->getFmax_mm(),
+            ':nb_points'=>$_GET["nb_points"],
+            ':stringDate'=>$selectedRecord->getDate(),
+            ':id'=>$selectedRecord->getId()));
+            echo "<script>window.location = window.location.pathname;</script>";
+          }
+        } else if($_GET['mode']=='Supprimer'){
+          $delete = $dbCnx->prepare("DELETE FROM parametre WHERE id=".$_GET['id']."");
+          $delete->execute();
         }
-
-      } else if($_GET['mode']=='Supprimer'){
-        $delete = $dbCnx->prepare("DELETE FROM parametre WHERE id=".$_GET['id']."");
-        $delete->execute();
       }
-    }
 
-    ?>
+      ?>
