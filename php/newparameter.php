13aa0a55ac290021a91d3836d9455a34ce756427
EmtekDzn <lagadecpa@gmail.com>
2018-04-27 13:17:14 +0200

    ct an komantèr

--- php/newparameter.php
+++ php/newparameter.php
@@ -1,6 +1,7 @@
 <?php
 require 'dbconnect.php';
 ?>
+
 <!--
 ********************************************************************************
 AJOUT PAR FORMULAIRE D'UNE NOUVELLE CAMBRURE
@@ -31,11 +32,10 @@ if (isset($_GET["corde"])) {
   $newParameter->tmax_prc_to_mm();
   $newParameter->fmax_prc_to_mm();
 
-
   // var_dump($newParameter);
 
   $addParameterQuery = $dbCnx->prepare("INSERT INTO parametre (libelle, corde, tmax_prc, tmax_mm, fmax_prc, fmax_mm, nb_points, date, fic_img, fic_csv) VALUES (:libelle, :corde, :tmax_prc, :tmax_mm, :fmax_prc, :fmax_mm, :nb_points, :date, NULL, NULL)");
-  // $addParameterQuery->execute(array(':libelle'=>$_GET["libelle"],':corde'=>$_GET["corde"],':tmax_prc'=>$_GET["tmax_prc"],':tmax_mm'=>$newParameter->getTmax_mm(),':fmax_prc'=>$_GET["tmax_prc"],':fmax_mm'=>$newParameter->getFmax_mm(),':nb_points'=>$_GET["nb_points"],':date'=>$newParameter->getDate()));
+  $addParameterQuery->execute(array(':libelle'=>$_GET["libelle"],':corde'=>$_GET["corde"],':tmax_prc'=>$_GET["tmax_prc"],':tmax_mm'=>$newParameter->getTmax_mm(),':fmax_prc'=>$_GET["tmax_prc"],':fmax_mm'=>$newParameter->getFmax_mm(),':nb_points'=>$_GET["nb_points"],':date'=>$newParameter->getDate()));
 
   $statement=$dbCnx->query("SELECT * FROM parametre WHERE libelle='".$_GET['libelle']."'");
   $parameters=$statement->fetchAll(PDO::FETCH_CLASS,'Parametre');
